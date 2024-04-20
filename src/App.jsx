c98ca101f3d263db8cd718e86e1e86c9f4e394de
Krii <kriilunaus16@gmail.com>
2024-04-20 11:55:43 +0200

    add page check

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,17 @@ import Loader from "./components/Loader/Loader.jsx";
 
 const App = () => {
 
-  const [photos, setPhotos] = useState([]);
+  const [photos, setPhotos] = useState([]);     // array with data
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [query, setQuery] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [imgId, setImgId] = useState("")
+  const [query, setQuery] = useState("");         // query for input
+  const [currentPage, setCurrentPage] = useState(1);    
+  const [modalIsOpen, setIsOpen] = useState(false);   
+  const [imgId, setImgId] = useState("");
+  const [newPagesAvaliable, setNewPagesAvaliable] = useState("true");
 
       useEffect(() => {
-        if (query !== "") {
+        if (query !== "" ) {
   
           async function fetchPhotos() {
             try {
@@ -28,12 +29,13 @@ const App = () => {
               setError(false)
               setLoading(true);
               
-              await new Promise(resolve => setTimeout(resolve, 1000));
+              
+              await new Promise(resolve => setTimeout(resolve, 1000));    // makes timeout for loader
               
               const data = await fetchPhotosWithQuery(query, currentPage);
-              
-              
-              setPhotos(prevPhotos => [...prevPhotos, ...data]);
+              setNewPagesAvaliable((data.total_pages >= currentPage))
+console.log(data)
+              setPhotos(prevPhotos => [...prevPhotos, ...data.results]);
               
             } catch (error) {
               setError(true);
@@ -46,14 +48,14 @@ const App = () => {
         }
         }, [query, currentPage])
         
-  const handleSubmit = inputValue => {
+  const handleSubmit = inputValue => {     // function on submit form
     setQuery(
-      inputValue.query);
-    setCurrentPage(1);
-    setPhotos([])
+      inputValue.query);                    //set query state
+    setCurrentPage(1);                      //set page to first
+    setPhotos([])                           // remove all from photos array
   }
 
-  const handleLoadMore = () => {
+  const handleLoadMore = () => {          // function for loadMoreBtn
   setCurrentPage(prevPage => prevPage + 1);
   };
   
@@ -75,9 +77,11 @@ const App = () => {
       <Loader loading={loading}/>
       {error && <ErrorElement />}
 
+
       {photos.length > 0 &&<ImageGallery  onModal={onModal}  photos={photos} />}
 
-      {query && <LoadMoreBtn onClick={handleLoadMore } />}
+      {query && newPagesAvaliable && <LoadMoreBtn onClick={handleLoadMore } />}
+      {!newPagesAvaliable && <p style={{ color: "white", padding: "50px"}}>There are no other photo. Try another request</p>}
       <ImageModal photos={photos} imgId={imgId} onClose={closeModal} modalIsOpen={modalIsOpen} />
     </>
   )
--- src/photos-api.js
+++ src/photos-api.js
@@ -7,7 +7,7 @@ const api_key = "Xq6Cb4oIUXC1RuptGDHSRwyh-ocDIUz4iU2ePjbzM-U";
  const fetchPhotosWithQuery = async (request, currentPage) => {
     const response = await axios.get(`https://api.unsplash.com/search/photos/?client_id=${api_key}&query=${request}&page=${currentPage}&per_page=4`)
 
-     return response.data.results;
+     return response.data;
      
 }
 export default fetchPhotosWithQuery;
